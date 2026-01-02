7e216e44f753984bd43fc5a50f010a0ec299ddd5
d3 <d3@d3s-MacBook-Pro.local>
2026-01-01 23:07:03 -0800

    Added AlienInvasion class

---
+++ alien_invasion.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    '''Overall class to manage game asserts and behavior.'''
+    pygame.init()
+
+    self.screen = pygame.display.set_mode((1200,800))
+    pygame.dispay.set_caption("Alien invasion")
+    
