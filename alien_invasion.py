fc3d0dc0f6f4d594301f954e0c223483d8691aca
d3 <d3@d3s-MacBook-Pro.local>
2026-01-02 01:38:47 -0800

    Added AlienInvasion class

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,8 +3,30 @@ import pygame
 
 class AlienInvasion:
     '''Overall class to manage game asserts and behavior.'''
-    pygame.init()
+    def __init__(self):
+        '''Initialize the game and create game resources.'''
+        pygame.init()
 
-    self.screen = pygame.display.set_mode((1200,800))
-    pygame.dispay.set_caption("Alien invasion")
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.dispay.set_caption("Alien invasion")
+
+    def run_game(self):
+        '''Start the main loop for the game'''
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                
+            #Make the most recently drawn screen visible.
+            pygame.display.flip()
+
     
+
+
+if __name__ == "__main__":
+    #make game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+    
+
